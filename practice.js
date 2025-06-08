d07309dfd07852e56c210afe439d10a44769a894
maivlys <122049862+maivlys@users.noreply.github.com>
2025-06-08 13:19:40 +0200

    testing long strings and negative keys

--- practice.js
+++ practice.js
@@ -45,8 +45,78 @@ class Calculator {
   }
 }
 
+function caesarCipher(str, key) {
+  if (typeof key !== "number") {
+    return "insert a key (number) to encrypt";
+  } else if (typeof str !== "string" || str === "") {
+    return "nothing to encrypt";
+  }
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let cipher = [];
+  let arr = str.split("");
+  let toPush = "";
+  arr.forEach((letter) => {
+    if (!alphabet.includes(letter.toLowerCase())) {
+      return cipher.push(letter);
+    }
+    let i = alphabet.indexOf(letter.toLowerCase());
+    key = key >= alphabet.length ? key % 26 : key;
+
+    if (i + key >= alphabet.length) {
+      toPush = isCapital(letter)
+        ? alphabet.at(i + key - alphabet.length).toUpperCase()
+        : alphabet.at(i + key - alphabet.length);
+    } else {
+      toPush = isCapital(letter)
+        ? alphabet.at(i + key).toUpperCase()
+        : alphabet.at(i + key);
+    }
+    cipher.push(toPush);
+  });
+
+  return cipher.join("");
+}
+
+function isCapital(letter) {
+  if (letter.toUpperCase() === letter) {
+    return true;
+  }
+  return false;
+}
+
+function analyzeArray() {}
+
 module.exports = {
   capitalize,
   reverseString,
   Calculator,
+  caesarCipher,
+  analyzeArray,
 };
--- practice.test.js
+++ practice.test.js
@@ -1,4 +1,10 @@
-const { capitalize, reverseString, Calculator } = require("./practice");
+const {
+  capitalize,
+  reverseString,
+  Calculator,
+  caesarCipher,
+  analyzeArray,
+} = require("./practice");
 
 test("capitalize", () => {
   expect(capitalize("string")).toBe("String");
@@ -32,4 +38,33 @@ test("calculator", () => {
   expect(calc.divide(-2, "")).toBe("insert 2 numbers");
   expect(calc.divide("string")).toBe("insert 2 numbers");
   expect(calc.divide(2, 0)).toBe("can not divide by zero");
+});
+
+test("caesarCipher", () => {
+  expect(caesarCipher("abc", 3)).toBe("def");
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+  expect(caesarCipher("", 3)).toBe("nothing to encrypt");
+  expect(caesarCipher("")).toBe("insert a key (number) to encrypt");
+  expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
+  expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+  expect(caesarCipher("abc", 0)).toBe("abc");
+  expect(caesarCipher("Hello, World!", 0)).toBe("Hello, World!");
+  expect(caesarCipher("abc", 26)).toBe("abc");
+  expect(caesarCipher("abc", 29)).toBe("def");
+  expect(caesarCipher("xyz", 52)).toBe("xyz");
+  expect(caesarCipher("   ", 5)).toBe("   ");
+  expect(caesarCipher("123", 5)).toBe("123");
+  expect(caesarCipher("!@#$%", 5)).toBe("!@#$%");
+  expect(caesarCipher("1aB2c!@", 1)).toBe("1bC2d!@");
+  expect(caesarCipher("Zebra-123.!", 5)).toBe("Ejgwf-123.!");
+  const longString =
+    "The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.";
+  const expectedLongString =
+    "Wkh txlfn eurzq ira mxpsv ryhu wkh odcb grj. Wkh txlfn eurzq ira mxpsv ryhu wkh odcb grj. Wkh txlfn eurzq ira mxpsv ryhu wkh odcb grj.";
+  expect(caesarCipher(longString, 3)).toBe(expectedLongString);
+  expect(caesarCipher("def", -3)).toBe("abc");
+  expect(caesarCipher("abc", -1)).toBe("zab");
+  expect(caesarCipher("Hello", -29)).toBe("Ebiil");
+  expect(caesarCipher("abc", -26)).toBe("abc");
+  expect(caesarCipher("def", -29)).toBe("abc");
 });
